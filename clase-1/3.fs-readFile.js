f40dc66d4a4805763f72cc61c294252d3b87aed6
Yami-san <jeanmarcopedraza@gmail.com>
2024-02-01 13:43:42 -0500

    change of dependency finally is working, i'm on windows

--- clase-1/3.fs-readFile.js
+++ clase-1/3.fs-readFile.js
@@ -1,13 +1,22 @@
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo...')
-fs.readFile('./archivo.txt', 'utf-8', (err, text) => { // <---- ejecutas este callback
-  console.log('primer texto:', text)
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => {
+  try {
+    // <---- ejecutas este callback
+    console.log('primer texto:', text)
+  } catch (error) {
+    console.error('Error al leer el directorio: ', err)
+  }
 })
 
 console.log('--> Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo...')
 fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
-  console.log('segundo texto:', text)
+  try {
+    console.log('segundo texto:', text)
+  } catch (error) {
+    console.error('Error al leer el directorio: ', err)
+  }
 })
--- clase-1/6.ls.js
+++ clase-1/6.ls.js
@@ -9,6 +9,5 @@ fs.readdir('.')
   .catch(err => {
     if (err) {
       console.error('Error al leer el directorio: ', err)
-      return;
     }
   })
